c499435cf0ddf8b1101e2bfe95a5b7254aecf78a
Bykov Slava <avel2k6@gmail.com>
2019-04-25 16:05:44 +0300

    update get_url method. It return array now

--- Sber.php
+++ Sber.php
@@ -12,9 +12,10 @@ class Sber
 				$bank_url       = self::$b_register.'?userName='.self::$b_username.'&password='.self::$b_pass.'&amount='.$bank_summ.'00&currency=643&language=ru&orderNumber='.$id_order.'&returnUrl='.$return_url.'';
 				$bank_response 	= file_get_contents($bank_url);
 				$b_obj		    	= json_decode($bank_response);
-				$target_url 	  = $b_obj->formUrl;
-        // Возвращаем путь, по которому надо перенаправить клиента на шлюз Сбербанка
-				return $target_url;
+				$return['target_url'] 	  = $b_obj->formUrl;
+        $return['orderId']        = $b_obj->orderId;
+        // Возвращаем путь, по которому надо перенаправить клиента на шлюз Сбербанка и ID заказа в системе банка
+				return $return;
 		}
 	static public function get_status($orderId){
 
